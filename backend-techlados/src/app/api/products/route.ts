4ce4d9db9a4b4f058a6e50bd4d6d9d448143e9ff
AlfonsoAguor <56021919+AlfonsoAguor@users.noreply.github.com>
2025-02-10 17:54:00 +0100

    last

--- backend-techlados/src/app/api/products/route.ts
+++ backend-techlados/src/app/api/products/route.ts
@@ -1,6 +1,7 @@
 import { connectDB } from "@/libs/mongodb";
 import { NextResponse } from "next/server";
 import { productSchema } from "@/schema/productSchema";
+import { Product } from "@/models/product";
 
 export async function POST(req: Request) {
     await connectDB();
@@ -8,7 +9,12 @@ export async function POST(req: Request) {
 
     try {
         const validatedData = productSchema.parse(body);
-        return NextResponse.json({ message: "Producto creado", data: validatedData });
+        const {name, price, stock, description} = validatedData;
+        
+        const prod = new Product({name, price, stock, description});
+        const prodSaved = await prod.save();
+
+        return NextResponse.json({ message: "Producto creado", data: prodSaved });
 
     } catch (error: any) {
         return NextResponse.json({ error: error.errors }, { status: 400 });
